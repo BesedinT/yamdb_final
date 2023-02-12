23ea5bd9d6e57326859cf47f34a463b185a8ef86
Анатолий Беседин <tolik777-87@mail.ru>
2023-02-12 14:21:39 +0400

    ver 1.2

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='p&l%385148kslhtyn^##a1)ilz@4zqj=rq
 
 DEBUG = False
 
-ALLOWED_HOSTS = os.getenv(['ALLOWED_HOSTS'], default=['158.160.16.72'])
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='158.160.16.72').split(',')
 
 
 INSTALLED_APPS = [
