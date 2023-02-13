9442374f2b43f1c9b57d285f646b9dbea1db1204
Анатолий Беседин <tolik777-87@mail.ru>
2023-02-13 22:40:55 +0400

    ver. 1.3

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -9,9 +9,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs')
 
-DEBUG = False
+DEBUG = os.getenv('DEBUG', default='FALSE')
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split(',')
 
 
 INSTALLED_APPS = [
@@ -62,15 +62,24 @@ TEMPLATES = [
 WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME', default='postgres'),
-        'USER': os.getenv('POSTGRES_USER', default='postgres'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
-        'HOST': os.getenv('DB_HOST', default='db'),
-        'PORT': os.getenv('DB_PORT', default='5432')
+if os.getenv('DATABASES') == 'sqlite':
+    data_default = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+else:
+    data_default = {
+    'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
+    'NAME': os.getenv('DB_NAME', default='postgres'),
+    'USER': os.getenv('POSTGRES_USER', default='postgres'),
+    'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
+    'HOST': os.getenv('DB_HOST', default='db'),
+    'PORT': os.getenv('DB_PORT', default='5432')
+    }
+
+
+DATABASES = {
+    'default': data_default
 }
 
 
